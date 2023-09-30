0d9aa04eebec21b7e542dac979f12f9e7ffdfba3
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 13:27:54 +0100

    version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,9 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 lua54 'yes'
 
+author 'RexShack#3041'
 description 'rsg-horses'
+version '1.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,35 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
--------------------------------------------------------------------------------
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-horses/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
 
 -- player horselantern
 RSGCore.Functions.CreateUseableItem("horselantern", function(source, item)
@@ -302,3 +331,8 @@ function GenerateHorseid()
     end
     return horseid
 end
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
