e5b825606bda5b54b8f76e499b0f860e0b87f8b5
RexshackGaming <rexshackuk@gmail.com>
2024-06-23 06:04:24 +0100

    remove old checker

--- server/server.lua
+++ server/server.lua
@@ -1,35 +1,6 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
 -----------------------------------------------------------------------
--- version checker
------------------------------------------------------------------------
-local function versionCheckPrint(_type, log)
-    local color = _type == 'success' and '^2' or '^1'
-
-    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
-end
-
-local function CheckVersion()
-    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-horses/main/version.txt', function(err, text, headers)
-        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
-
-        if not text then 
-            versionCheckPrint('error', 'Currently unable to run a version check.')
-            return 
-        end
-
-        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
-        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
-        
-        if text == currentVersion then
-            versionCheckPrint('success', 'You are running the latest version.')
-        else
-            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
-        end
-    end)
-end
-
------------------------------------------------------------------------
 
 -- find horse command
 RSGCore.Commands.Add("findhorse", "find where your horses are stored", {}, false, function(source)
@@ -449,8 +420,3 @@ UpkeepInterval = function()
 end
 
 SetTimeout(Config.CheckCycle * (60 * 1000), UpkeepInterval)
-
---------------------------------------------------------------------------------------------------
--- start version check
---------------------------------------------------------------------------------------------------
-CheckVersion()
