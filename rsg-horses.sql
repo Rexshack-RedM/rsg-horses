40fc7718fac19383f611b71c68b4fc6e62ef6809
RexshackGaming <rexshackuk@gmail.com>
2024-09-26 13:00:13 +0100

    new sql

---
+++ rsg-horses.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS `player_horses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stable` varchar(50) NOT NULL,
+  `citizenid` varchar(50) NOT NULL,
+  `horseid` varchar(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `horse` varchar(50) DEFAULT NULL,
+  `dirt` int(11) DEFAULT 0,
+  `horsexp` int(11) DEFAULT 0,
+  `components` LONGTEXT NOT NULL,
+  `gender` varchar(11) NOT NULL,
+  `wild` varchar(11) DEFAULT NULL,
+  `active` tinyint(4) DEFAULT 0,
+  `born` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
