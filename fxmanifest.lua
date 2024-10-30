8281b910101b8b8b7ee3fe8c00c7e0a9d1fb2996
RexshackGaming <rexshackuk@gmail.com>
2024-10-30 15:57:25 +0000

    v2 version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 game 'rdr3'
 
 description 'rsg-horses'
-version '1.4.8'
+version '2.0.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
